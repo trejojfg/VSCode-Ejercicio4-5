3cb78f1ee3e732aecd4fc4a608796b90eec9444f
TREJO <trejojfrancisco@gmail.com>
2023-08-14 13:36:37 +0200

    Primera Subida

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ejercicio4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hacer un programa para ingresar 4 números. Luego analizar e informar por pantalla 
+            // si los mismos se encuentran ordenados de forma decreciente .
+            
+            int a, b, c, d;
+
+            Console.WriteLine("Ingresar el 1º numero");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar el 1º numero");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar el 1º numero");
+            c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar el 1º numero");
+            d = int.Parse(Console.ReadLine());
+            
+            if (a > b && b > c && c > d)
+                Console.WriteLine("Los numeros estan ordenados decrecientemente");
+            else
+                Console.WriteLine("Los numeros no estan ordenados de forma decreciente");
+
+            Console.WriteLine("Fin del programa");
+        }
+    }
+}
